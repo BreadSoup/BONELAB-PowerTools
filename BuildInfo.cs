a1dadf3f8c28b5aac77dd1b7c35684dd462e848c
BreadSoup <79678268+BreadSoup@users.noreply.github.com>
2023-07-30 11:28:02 -0400

    Initial commit

---
+++ BuildInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace bonelab_template
+{
+    internal partial class Main
+    {
+        internal const string Name = "Mod Name";
+        internal const string Description = "Mod Description";
+        internal const string Author = "Mod Author";
+        internal const string Company = "Mod Company";
+        internal const string Version = "0.0.1";
+        internal const string DownloadLink = "Link";
+    }
+}
