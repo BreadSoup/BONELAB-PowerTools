ac5a1c708d7c201035f30a52426f7b74a1e2521b
BreadSoup <79678268+BreadSoup@users.noreply.github.com>
2023-08-29 22:40:46 -0400

    v1.0.0

--- Tools/VaultingToggle.cs
+++ Tools/VaultingToggle.cs
@@ -1,32 +1,22 @@
 ﻿using HarmonyLib;
 using MelonLoader;
-using Mono.CSharp;
 using SLZ.Rig;
-using UnityEngine;
 
 namespace PowerTools.Tools
 {
     [HarmonyPatch(typeof(PhysicsRig), "CheckDangle")]
     public class VaultingToggle
     {
-        private static MelonPreferences_Entry<bool> MelonPrefEnabled { get; set; }
         private static bool VaultingToggleIsEnabled { get; set; }
         private static MelonPreferences_Entry<bool> MelonPrefVaultingToggle { get; set; }
-        public static bool VaultingToggleSetting;
-        public static bool IsDefaultSet { get; set; }
 
         public static void MelonPreferencesCreator()
         {
-            MelonPrefEnabled = Main.MelonPrefCategory.CreateEntry("VaultingToggleIsEnabled", false);
-            MelonPrefVaultingToggle = Main.MelonPrefCategory.CreateEntry("Vaulting Toggle", false);
-            if (MelonPrefEnabled != null)
-            {
-                VaultingToggleIsEnabled = MelonPrefEnabled.Value;
-            }
+            MelonPrefVaultingToggle = Main.MelonPrefCategory.CreateEntry("Vaulting Toggle", true);
 
             if (MelonPrefVaultingToggle != null)
             {
-                VaultingToggleSetting = MelonPrefVaultingToggle.Value;
+                VaultingToggleIsEnabled = MelonPrefVaultingToggle.Value;
             }
         }
 
@@ -34,12 +24,12 @@ namespace PowerTools.Tools
         {
             var vaultingToggle = Main.Category.CreateCategory("Vaulting Toggle", "#7526fc"); 
 
-            vaultingToggle.CreateBoolElement("Mod Toggle", Color.yellow, VaultingToggleIsEnabled, OnSetEnabled);
+            vaultingToggle.CreateBoolElement("Vaulting", "#6d45ff", VaultingToggleIsEnabled, OnSetEnabled);
         }
         
         public static bool Prefix(PhysicsRig __instance, ref bool __result)
         {
-            if(VaultingToggleIsEnabled)
+            if(!VaultingToggleIsEnabled)
             {
                 __result = false;
                 return false;
@@ -53,7 +43,6 @@ namespace PowerTools.Tools
         private static void OnSetEnabled(bool value)
         {
             VaultingToggleIsEnabled = value;
-            MelonPrefEnabled.Value = value;
             Main.MelonPrefCategory.SaveToFile(false);
         }
     }
